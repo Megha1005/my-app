6375ccf5da04c2277c0f1a222b69c76833a06741
Megha1005 <mbabariya@mail.sfsu.edu>
2020-07-08 13:53:58 -0700

    fix code

---
+++ build/precache-manifest.d8a9dcc6486775a40782ea5cbb7382b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad66af1a7c9a80771b0772e8d5dcc26",
+    "url": "/Megha1005.github.io/my-app/index.html"
+  },
+  {
+    "revision": "e5b0a7a2deaf2aca3fd1",
+    "url": "/Megha1005.github.io/my-app/static/css/main.4a195fe7.chunk.css"
+  },
+  {
+    "revision": "e0aadac87e7ac18f81e4",
+    "url": "/Megha1005.github.io/my-app/static/js/2.6d013ad0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Megha1005.github.io/my-app/static/js/2.6d013ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b0a7a2deaf2aca3fd1",
+    "url": "/Megha1005.github.io/my-app/static/js/main.16871408.chunk.js"
+  },
+  {
+    "revision": "326b648ecc59accc1f9d",
+    "url": "/Megha1005.github.io/my-app/static/js/runtime-main.101e2109.js"
+  },
+  {
+    "revision": "e448a28dcb5019d96f58f2160017ce72",
+    "url": "/Megha1005.github.io/my-app/static/media/back.e448a28d.jpg"
+  }
+]);
